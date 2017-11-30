348092b025775f725bc4da0070150eed1b24ed6f
Bharath <bharathnepo@gmail.com>
2017-12-01 00:41:21 +0530

    Diamond class added with isosceles and diamond output verified

---
+++ src/Diamond.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Diamond {
+
+    public static void main(String[] args)
+    {
+        int n;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input n:");
+        n = Integer.parseInt(in.next());
+        System.out.println("Printing Isosceles Triangle:");
+        for(int i=0,j=1;i<n;i++,j=j+2) {
+            for (int k = 0; k < j; k++)
+                System.out.print("*");
+            System.out.println("");
+
+        }
+
+        System.out.println("Printing Diamond:");
+        for(int i=0,j=1;i<n;i++,j=j+2) {
+            for (int k = 0; k < j; k++)
+                System.out.print("*");
+            System.out.println("");
+            if(i==n-1)
+            {
+                while(j>=1) {
+                    j=j-2;
+                    for (int k = 0; k < j; k++)
+                        System.out.print("*");
+
+                    System.out.println("");
+                }
+
+            }
+
+            System.out.println("Printing name Diamond:");
+            for(int i=0,j=1;i<n;i++,j=j+2) {
+                for (int k = 0; k < j; k++)
+                    System.out.print("*");
+                System.out.println("");
+                if(i==n-1)
+                {
+                    while(j>=1) {
+                        j=j-2;
+                        for (int k = 0; k < j; k++)
+                            System.out.print("*");
+
+                        System.out.println("");
+                    }
+
+                }
+
+
+            }
+
+    }
+
+
+
+}
